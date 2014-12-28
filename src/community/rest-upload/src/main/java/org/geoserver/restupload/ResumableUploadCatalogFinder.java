9568fa64af07e3c1181ae0fd42581a097358ba1d
Sandro Salari <sandro.salari@gmail.com>
2014-12-28 15:08:47 +0100

    Added rest upload module

---
+++ src/community/rest-upload/src/main/java/org/geoserver/restupload/ResumableUploadCatalogFinder.java
@@ -0,0 +1,29 @@
+/* (c) 2014-2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.restupload;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.rest.AbstractCatalogFinder;
+import org.restlet.data.Method;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Resource;
+
+public class ResumableUploadCatalogFinder extends AbstractCatalogFinder{
+
+    protected ResumableUploadCatalogFinder(Catalog catalog) {
+        super(catalog);
+    }
+
+    @Override
+    public Resource findTarget(Request request, Response response) {
+        if (request.getMethod() == Method.GET ) {
+            return new ResumableUploadCatalogResource(getContext(),request,response,catalog);
+        }
+        return new ResumableUploadCatalogResource( null, request, response, catalog );
+    }
+
+}
---
+++ src/community/rest-upload/src/main/java/org/geoserver/restupload/ResumableUploadResourceManager.java
@@ -0,0 +1,16 @@
+/* (c) 2014-2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.restupload;
+
+import java.util.logging.Logger;
+
+import org.geotools.util.logging.Logging;
+
+public class ResumableUploadResourceManager {
+
+    private static final Logger LOGGER = Logging.getLogger(ResumableUploadResourceManager.class);
+
+}
