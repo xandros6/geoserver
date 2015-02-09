6324cc55ec67fbb5d39c6e216d5256ed46336faf
Sandro Salari <sandro.salari@gmail.com>
2015-02-09 13:01:17 +0100

    Added PPIO type and managed BINARY defualt value in deserialization

--- src/extension/ogr/src/main/java/org/geoserver/wfs/response/OgrFormat.java
+++ src/extension/ogr/src/main/java/org/geoserver/wfs/response/OgrFormat.java
@@ -1,10 +1,11 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
 package org.geoserver.wfs.response;
 
+import java.io.ObjectStreamException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -37,27 +38,55 @@ public class OgrFormat {
     public List<String> options;
     
     /**
-     * If the output is a single file that can be streamed back. In that case we also need
-     * to know the mime type
+     * The type of format, used to instantiate the correct converter
+     */
+    public OgrType type;
+
+    /**
+     * If the output is a single file that can be streamed back. In that case we also need to know the mime type
      */
     public boolean singleFile;
-    
+
     /**
      * The mime type of the single file output
      */
     public String mimeType;
 
-    public OgrFormat(String ogrFormat, String formatName, String fileExtension, boolean singleFile, 
-            String mimeType, String... options) {
+    public OgrFormat(String ogrFormat, String formatName, String fileExtension, boolean singleFile,
+            String mimeType, OgrType type, String... options) {
         this.ogrFormat = ogrFormat;
         this.formatName = formatName;
         this.fileExtension = fileExtension;
         this.singleFile = singleFile;
         this.mimeType = mimeType;
-        if(options != null) {
+        this.type = type;
+        if (options != null) {
             this.options = new ArrayList<String>(Arrays.asList(options));
         }
     }
 
-    
+    public OgrFormat(String ogrFormat, String formatName, String fileExtension, boolean singleFile,
+            String mimeType, String... options) {
+        this(ogrFormat, formatName, fileExtension, singleFile, mimeType, OgrType.BINARY, options);
+    }
+
+    /**
+     * There is one major flaw in XStream. Unfortunately it has no way of telling if a field or attribute should get any default value if not present
+     * in the xml file. Because constructor is not being invoked we cannot set the value there. Neither setting the value in field definition will
+     * work. The resulting instance will always have zero or null values in the fields.
+     *
+     * The only way of setting the desired default value is using the following method. It is called during deserialization process and here we can
+     * check if the field value is null. If yes it means that it's tag is not present and we can set the default value if needed.
+     *
+     * @return this
+     * @throws ObjectStreamException
+     */
+
+    private Object readResolve() throws ObjectStreamException {
+        if (this.type == null) {
+            this.type = OgrType.BINARY;
+        }
+        return this;
+    }
+
 }
