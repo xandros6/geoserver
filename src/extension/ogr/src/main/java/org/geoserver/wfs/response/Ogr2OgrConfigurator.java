fc79d5697f1c0c693cd87b67fad03e2ca192ef5f
Sandro Salari <sandro.salari@gmail.com>
2015-02-12 12:13:48 +0100

    Removed changes

--- src/extension/ogr/src/main/java/org/geoserver/wfs/response/Ogr2OgrConfigurator.java
+++ src/extension/ogr/src/main/java/org/geoserver/wfs/response/Ogr2OgrConfigurator.java
@@ -1,4 +1,4 @@
-/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -76,16 +76,12 @@ public class Ogr2OgrConfigurator implements ApplicationListener<ContextClosedEve
                             "Could not find/load the ogr2ogr.xml configuration file, using internal defaults");
         }
 
-        if(of.getOgrExecutable() == null){
-            of.setOgrExecutable(configuration.ogr2ogrLocation);
-        }
-        if(of.getGdalData() == null){
-            of.setGdalData(configuration.gdalData);
-        }
-        of.clearFormats();
         // let's load the configuration
-        OGRWrapper wrapper = new OGRWrapper(of.getOgrExecutable(), of.getGdalData());
+        OGRWrapper wrapper = new OGRWrapper(configuration.ogr2ogrLocation, configuration.gdalData);
         Set<String> supported = wrapper.getSupportedFormats();
+        of.setOgrExecutable(configuration.ogr2ogrLocation);
+        of.setGdalData(configuration.gdalData);
+        of.clearFormats();
         for (OgrFormat format : configuration.formats) {
             if (supported.contains(format.ogrFormat)) {
                 of.addFormat(format);
