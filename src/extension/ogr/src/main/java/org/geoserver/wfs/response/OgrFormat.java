3efbaae909fd2669ea1c33a3491b3306738c749d
Sandro Salari <sandro.salari@gmail.com>
2015-02-12 12:16:02 +0100

    Removed xstream default value managment

--- src/extension/ogr/src/main/java/org/geoserver/wfs/response/OgrFormat.java
+++ src/extension/ogr/src/main/java/org/geoserver/wfs/response/OgrFormat.java
@@ -5,14 +5,13 @@
  */
 package org.geoserver.wfs.response;
 
-import java.io.ObjectStreamException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * Parameters defining an output format generated using ogr2ogr from
- * either a GML dump 
+ * Parameters defining an output format generated using ogr2ogr from either a GML dump
+ *
  * @author Andrea Aime - OpenGeo
  *
  */
@@ -21,22 +20,22 @@ public class OgrFormat {
      * The -f parameter
      */
     public String ogrFormat;
-    
+
     /**
      * The GeoServer output format name
      */
     public String formatName;
-    
+
     /**
      * The extension of the generated file, if any (shall include a dot, example, ".tab")
      */
     public String fileExtension;
-    
+
     /**
      * The options that will be added to the command line
      */
     public List<String> options;
-    
+
     /**
      * The type of format, used to instantiate the correct converter
      */
@@ -63,30 +62,14 @@ public class OgrFormat {
         if (options != null) {
             this.options = new ArrayList<String>(Arrays.asList(options));
         }
+        if (type == null) {
+            this.type = OgrType.BINARY;
+        }
     }
 
     public OgrFormat(String ogrFormat, String formatName, String fileExtension, boolean singleFile,
             String mimeType, String... options) {
         this(ogrFormat, formatName, fileExtension, singleFile, mimeType, OgrType.BINARY, options);
-    }
-
-    /**
-     * There is one major flaw in XStream. Unfortunately it has no way of telling if a field or attribute should get any default value if not present
-     * in the xml file. Because constructor is not being invoked we cannot set the value there. Neither setting the value in field definition will
-     * work. The resulting instance will always have zero or null values in the fields.
-     *
-     * The only way of setting the desired default value is using the following method. It is called during deserialization process and here we can
-     * check if the field value is null. If yes it means that it's tag is not present and we can set the default value if needed.
-     *
-     * @return this
-     * @throws ObjectStreamException
-     */
-
-    private Object readResolve() throws ObjectStreamException {
-        if (this.type == null) {
-            this.type = OgrType.BINARY;
-        }
-        return this;
     }
 
 }
